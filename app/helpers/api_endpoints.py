b6dda12adf07d35b12798b374ac1aeb818afb526
Adam Hoštička <hostiada@fit.cvut.cz>
2023-01-03 06:40:09 +0100

    semestral-helpers: fixed typo

--- app/helpers/api_endpoints.py
+++ app/helpers/api_endpoints.py
@@ -3,7 +3,7 @@ API endpoints of the Chess.com API (https://www.chess.com/news/view/published-da
 """
 
 # TODO
-REQUEST_HEADER = {
+REQUEST_HEADERS = {
     'Host': 'api.chess.com',
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
