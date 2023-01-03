c8c8ded88a738632e572546ee4dd28351b7191f0
Adam Hoštička <hostiada@fit.cvut.cz>
2023-01-03 08:40:07 +0100

    semestral-fetch-data: fetch_country class to fetch country details

---
+++ app/src/fetch_data_from_chesscom_api/fetch_country.py
@@ -0,0 +1,19 @@
+"""
+Fetch country information from Chess.com API and save it to csv file
+"""
+
+from app.helpers.iso_codes import ISO_CODES
+from app.helpers.api_endpoints import COUNTRY_ENDPOINT
+from app.src.fetch_data_from_chesscom_api.fetch_base import FetchBase
+
+
+class FetchCountry(FetchBase):
+    FILE_NAME = 'countries.csv'
+
+    def fetch_data(self):
+        for iso_code in ISO_CODES:
+            self.data.append(self.fetch_item(COUNTRY_ENDPOINT.format(iso=iso_code)))
+
+
+if __name__ == '__main__':
+    FetchCountry().run()
