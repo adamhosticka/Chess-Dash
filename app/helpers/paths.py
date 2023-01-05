f23ae10e6845dd9ca9b1c86e1f64da127b03cc5e
Adam Hoštička <hostiada@fit.cvut.cz>
2023-01-05 18:15:10 +0100

    semestral: paths fix

--- app/helpers/paths.py
+++ app/helpers/paths.py
@@ -2,5 +2,5 @@
 
 import os
 
-ROOT_DIR = os.path.dirname(os.path.abspath(os.path.join(os.getcwd(), os.pardir)))
+ROOT_DIR = os.path.dirname(os.path.abspath(os.path.join(os.path.abspath(__file__), os.pardir)))
 DATA_DIR = os.path.join(ROOT_DIR, 'data')
