553adda4e7226d18530f9d6f30f64143c24b8d57
Adam Hoštička <hostiada@fit.cvut.cz>
2023-01-07 20:54:34 +0100

    semestral-tests: added util and test - dataframes_equal

---
+++ app/utils/compare_dataframes.py
@@ -0,0 +1,18 @@
+"""Compare dataframes."""
+from typing import Union
+
+import pandas as pd
+
+
+def dataframes_equal(df1: pd.DataFrame, df2: pd.DataFrame, sort_by: Union[str, list]) -> bool:
+    """Compare dataframe regardles of row order.
+
+    :param: pd.DataFrame df1: First dataframe.
+    :param: pd.DataFrame df2: Second dataframe.
+    :param: str sort_by: Column name to sort by.
+    :return: True if dataframes are equals regardles of row order, False otherwise.
+    :rtype: bool.
+    """
+    sorted_df1 = df1.sort_values(sort_by).reset_index(drop=True)
+    sorted_df2 = df2.sort_values(sort_by).reset_index(drop=True)
+    return sorted_df1.equals(sorted_df2)
---
+++ test/utils/test_compare_dataframes.py
@@ -0,0 +1,52 @@
+"""Test compare dataframes module."""
+from typing import Union
+
+import pytest
+import pandas as pd
+
+from app.utils.compare_dataframes import dataframes_equal
+
+
+@pytest.mark.parametrize(
+    ['df1', 'df2', 'sort_by', 'expected'],
+    [
+        (
+            pd.DataFrame({
+                'name': ['Adam', 'Peter', 'Janka'],
+                'prijmeni': ['Hosticka', 'Peter', 'Mala']
+            }),
+            pd.DataFrame({
+                'name': ['Peter', 'Adam', 'Janka'],
+                'prijmeni': ['Peter', 'Hosticka', 'Mala']
+            }),
+            'prijmeni',
+            True
+        ),
+        (
+            pd.DataFrame({
+                'name': ['Adam', 'Peter', 'Janka'],
+                'prijmeni': ['Hosticka', 'Peter', 'Mala']
+            }),
+            pd.DataFrame({
+                'name': ['Peter', 'Adam', 'Janka'],
+                'prijmeni': ['Hosticka', 'Peter', 'Mala']
+            }),
+            'prijmeni',
+            False
+        ),
+        (
+            pd.DataFrame({
+                'count': [1, 1, 2, 1, 3, 2],
+                'type': ['x', 'y', 'y', 'z', 'x', 's']
+            }),
+            pd.DataFrame({
+                'count': [2, 1, 1, 1, 3, 2],
+                'type': ['s', 'z', 'y', 'x', 'x', 'y']
+            }),
+            ['count', 'type'],
+            True
+        )
+    ]
+)
+def test_dataframes_equal(df1: pd.DataFrame, df2: pd.DataFrame, sort_by: Union[str, list], expected: bool):
+    assert dataframes_equal(df1, df2, sort_by) == expected
