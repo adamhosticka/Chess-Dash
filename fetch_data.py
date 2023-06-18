bfdea8a0a5c17d0284b1226dacfbf2a1d248c14f
Adam Hosticka <hostiada@fit.cvut.cz>
2023-06-19 01:11:16 +0200

    semestral: added short fetch data option and updated switch cases

--- fetch_data.py
+++ fetch_data.py
@@ -42,8 +42,9 @@ def fetch_item(data_class, print_info: bool, cnt: int = 1):
         print(f"Fetching {get_data_class_name(data_class)} took {time.time() - start_time} seconds")
 
 
-def fetch_data(fetch_all_and_reformat: bool, fetch_countries: bool, fetch_country_players: bool,
-               hundreds_of_players: int, hundreds_of_games: int, reformat_and_save: bool, disable_print_info: bool):
+def fetch_data(fetch_all_and_reformat: bool, fetch_all_and_reformat_short: bool, fetch_countries: bool,
+               fetch_country_players: bool, hundreds_of_players: int, hundreds_of_games: int,
+               reformat_and_save: bool, disable_print_info: bool):
     """Fetch data from Chess.com API based on input switches.
 
     :param: bool fetch_countries: Fetch countries switch.
@@ -51,8 +52,10 @@ def fetch_data(fetch_all_and_reformat: bool, fetch_countries: bool, fetch_countr
     :param: int hundreds_of_players: Number of hundreds of players to fetch.
     :param: int hundreds_of_games: Number of hundreds of games to fetch.
     :param: bool reformat_and_save: Reformat and save games switch.
-    :param: bool print_info: Print fetching info switch.
+    :param: bool disable_print_info: Disable print info switch.
     """
+    print_info = not disable_print_info
+
     if fetch_all_and_reformat:
         fetch_countries = True
         fetch_country_players = True
@@ -60,23 +63,30 @@ def fetch_data(fetch_all_and_reformat: bool, fetch_countries: bool, fetch_countr
         hundreds_of_games = 5
         reformat_and_save = True
 
-    if disable_print_info:
+    if fetch_all_and_reformat_short:
+        fetch_countries = True
+        fetch_country_players = True
+        hundreds_of_players = 2
+        hundreds_of_games = 1
+        reformat_and_save = True
+
+    if print_info:
         print("Fetching...")
 
     if fetch_countries:
-        fetch_item(FetchCountry, disable_print_info)
+        fetch_item(FetchCountry, print_info)
 
     if fetch_country_players:
-        fetch_item(FetchCountryPlayer, disable_print_info)
+        fetch_item(FetchCountryPlayer, print_info)
 
     if hundreds_of_players > 0:
-        fetch_item(FetchPlayer, disable_print_info, hundreds_of_players)
+        fetch_item(FetchPlayer, print_info, hundreds_of_players)
 
     if hundreds_of_games > 0:
-        fetch_item(FetchGame, disable_print_info, hundreds_of_games)
+        fetch_item(FetchGame, print_info, hundreds_of_games)
 
     if reformat_and_save:
-        if disable_print_info:
+        if print_info:
             print("Reformating games...")
         save_reformated_games()
 
@@ -88,6 +98,11 @@ if __name__ == '__main__':
                         action='store_true',
                         help="Fetch countries, players (1000) and games (500) - then reformat them. "
                              "Takes around twenty minutes.")
+    parser.add_argument("-fars", "--fetch-all-and-reformat-short",
+                        default=False,
+                        action='store_true',
+                        help="Fetch countries, players (200) and games (100) - then reformat them. "
+                             "Takes around five minutes.")
     parser.add_argument("-fc", "--fetch-countries",
                         default=False,
                         action='store_true',
@@ -98,12 +113,10 @@ if __name__ == '__main__':
                         help="Fetch country players")
     parser.add_argument("-FP", "--hundreds-of-players",
                         default=0,
-                        choices=range(0, 101),
                         type=int,
                         help="Number of hundreds of players to fetch")
     parser.add_argument("-FG", "--hundreds-of-games",
                         default=0,
-                        choices=range(0, 51),
                         type=int,
                         help="Number of hundreds of players to fetch games from (each player can have multiple games)")
     parser.add_argument("-r", "--reformat-and-save",
@@ -111,7 +124,7 @@ if __name__ == '__main__':
                         action='store_true',
                         help="Reformat fetched games after fetching and save them to another file")
     parser.add_argument("-p", "--disable-print-info",
-                        default=True,
+                        default=False,
                         action='store_true',
                         help="Disable printing fetching information")
     args = parser.parse_args()
